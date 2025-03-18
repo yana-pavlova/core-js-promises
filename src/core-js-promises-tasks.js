940b128dd31226ddceb2ea4fff5f83063a1f7c6d
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-18 13:52:41 +0300

    feat: implement getAllResult function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -109,8 +109,15 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      }
+      return null;
+    });
+  });
 }
 
 /**

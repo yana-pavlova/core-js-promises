232f6b8be0d12992556471abec14d0bd46236d6f
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-18 13:53:55 +0300

    feat: implement queuePromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(
+    (acc, promise) =>
+      acc.then((result) => promise.then((value) => `${result}${value}`)),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
